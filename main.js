1809acde9ee856f44eb990737ff1497a15c40121
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-06-28 10:17:37 +0200

    Create classes

--- main.js
+++ main.js
@@ -1,60 +1,95 @@
+// const addBook = document.querySelector('.classBook');
+// const title = document.querySelector('#bookTitle');
+// const author = document.querySelector('#bookAuthor');
+// const btn = document.querySelector('.submit');
+
+// // empty library array for storing book as object
+// const library = JSON.parse(localStorage.getItem('books') || '[]');
+
+// // existing entries
+// const existingEntries = JSON.parse(localStorage.getItem('books') || '[]');
+
+// // Update User Interface dynamically
+
+// const updateUI = () => {
+//   addBook.innerHTML = '';
+//   library.forEach((data, index) => {
+//     const classBook = document.createElement('div');
+//     classBook.classList.add('bookItem');
+//     const newBookTitle = document.createElement('p');
+//     newBookTitle.textContent = `${data.title}`;
+//     const newBookAuthor = document.createElement('p');
+//     newBookAuthor.textContent = `${data.author}`;
+//     const button = document.createElement('button');
+//     button.textContent = 'Remove';
+//     const separator = document.createElement('hr');
+
+//     // remove book function for remove button
+//     const removeBook = function () {
+//       library.splice(this, 1);
+//       localStorage.setItem('books', JSON.stringify(library));
+//       updateUI(library);
+//     };
+
+//     button.addEventListener('click', removeBook.bind(index));
+
+//     classBook.appendChild(newBookTitle);
+//     classBook.appendChild(newBookAuthor);
+//     classBook.appendChild(button);
+//     classBook.appendChild(separator);
+
+//     addBook.appendChild(classBook);
+//   });
+// };
+
+// updateUI();
+// // add new books in the library and show updated library
+
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+
+//   if (title.value && author.value) {
+//     const book = { title: title.value, author: author.value };
+//     library.push(book);
+//     updateUI(library);
+//     title.value = '';
+//     author.value = '';
+//     existingEntries.push(book);
+//     localStorage.setItem('books', JSON.stringify(existingEntries));
+//   }
+// });
+
+
+/********************IMPLEMENT USING CLASS START HERE ***************/
+
 const addBook = document.querySelector('.classBook');
 const title = document.querySelector('#bookTitle');
 const author = document.querySelector('#bookAuthor');
 const btn = document.querySelector('.submit');
 
-// empty library array for storing book as object
-const library = JSON.parse(localStorage.getItem('books') || '[]');
 
-// existing entries
-const existingEntries = JSON.parse(localStorage.getItem('books') || '[]');
+//class used to create array 
 
-// Update User Interface dynamically
-
-const updateUI = () => {
-  addBook.innerHTML = '';
-  library.forEach((data, index) => {
-    const classBook = document.createElement('div');
-    classBook.classList.add('bookItem');
-    const newBookTitle = document.createElement('p');
-    newBookTitle.textContent = `${data.title}`;
-    const newBookAuthor = document.createElement('p');
-    newBookAuthor.textContent = `${data.author}`;
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-    const separator = document.createElement('hr');
-
-    // remove book function for remove button
-    const removeBook = function () {
-      library.splice(this, 1);
-      localStorage.setItem('books', JSON.stringify(library));
-      updateUI(library);
-    };
-
-    button.addEventListener('click', removeBook.bind(index));
-
-    classBook.appendChild(newBookTitle);
-    classBook.appendChild(newBookAuthor);
-    classBook.appendChild(button);
-    classBook.appendChild(separator);
-
-    addBook.appendChild(classBook);
-  });
-};
-
-updateUI();
-// add new books in the library and show updated library
-
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (title.value && author.value) {
-    const book = { title: title.value, author: author.value };
-    library.push(book);
-    updateUI(library);
-    title.value = '';
-    author.value = '';
-    existingEntries.push(book);
-    localStorage.setItem('books', JSON.stringify(existingEntries));
+class Array {
+  constructor() {
+    this.content=[];
   }
-});
+}
+
+//create books array using class Array 
+
+let books =new Array;
+books.content.push("Chrispaix")
+console.log(books.content);
+
+
+class Book {
+  constructor(title,author){
+    this.title=title,
+    this.author=author,
+    this.display = function (){
+      console.log(`${this.title} by ${this.author}`);
+    }
+ 
+  }
+}
